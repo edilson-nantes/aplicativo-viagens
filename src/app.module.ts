7881827d510bd2deae3c4998ee48e5ebe12f707b
Edilson Nantes Junior <nantes.junior@gmail.com>
2024-11-26 16:06:54 -0300

    feat: criação dos modulos Driver e Ride, instalação de dependencias orm e postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { RideModule } from './ride/ride.module';
+import { DriverModule } from './driver/driver.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [RideModule, DriverModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
---
+++ src/ride/ride.controller.ts
@@ -0,0 +1,6 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('ride')
+export class RideController {
+
+}
